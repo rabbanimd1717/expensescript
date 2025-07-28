13a02c925ace84699469e9eb7d719376f7d3b507
rabbani <rabbani@gmail.com>
2025-07-28 16:56:19 +0530

    expense mysql server project

---
+++ 1.Mysql.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+VALIDATE_FUN(){
+    if [ $1 -ne 0 ]
+then
+    echo "$2 is FAILURE"
+    exit 1
+else
+    echo "$2 is SUCCESS"
+fi
+}
+
+
+if [ USER_ID -eq 0 ]
+then 
+    echo "This is SUPER USER GOT TO INSTALL PACKAGES"
+else
+    echo "THIS IS NOT SUPER USER PLEASE STOP OF THIS TO INSTALL PACKAGES"
+    exit 1
+fi
+
+dnf install mysql-server -y
+VALIDATE_FUN $? "INSTALLING MYSQL PACKAGE"
+
+systemctl enable mysqld
+VALIDATE_FUN $? "Mysql system enabaled"
+
+systemctl start mysqld
+VALIDATE_FUN $? "Mysql is start"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE_FUN $? "Password setup to enter the db"
