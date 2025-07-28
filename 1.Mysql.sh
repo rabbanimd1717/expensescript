68e667a91636972ab7c9da2b0cb180062f3a0736
rabbani <rabbani@gmail.com>
2025-07-28 18:08:10 +0530

    expense mysql server project

--- 1.Mysql.sh
+++ 1.Mysql.sh
@@ -17,10 +17,10 @@ N=\e"[0"
 VALIDATE_FUN(){
     if [ $1 -ne 0 ]
     then
-        echo "$2 is $R FAILURE $N"
+        echo -e "$2 is $R FAILURE $N"
         exit 1
     else
-        echo "$2 is $G SUCCESS $N"
+        echo -e "$2 is $G SUCCESS $N"
     fi
 }
 
