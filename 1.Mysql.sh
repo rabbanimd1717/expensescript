f70f49fdf4e09b0ca818a4e8ae3c42e83cf29f93
rabbani <rabbani@gmail.com>
2025-07-29 17:42:03 +0530

    expense mysql server project

--- 1.Mysql.sh
+++ 1.Mysql.sh
@@ -45,7 +45,7 @@ VALIDATE_FUN $? "START MYSQL"
 
 # mysql_secure_installation --set-root-pass ExpenseApp@1 >> $LOG_FILE
 
-mysql -h 172.31.45.100 -uroot -p${DB_PASSWORD} -e "SHOW DATABASES;"
+mysql -h 172.31.45.100 -uroot -p${DB_PASSWORD} -e 'SHOW DATABASES;'
 
 if [ $? -eq 0 ]
 then
