c819703cd8df7abe5aeef934967ae18d6ffbb54c
rabbani <rabbani@gmail.com>
2025-07-29 17:55:55 +0530

    expense mysql server project

--- 1.Mysql.sh
+++ 1.Mysql.sh
@@ -1,61 +1,61 @@
 #!/bin/bash
 
-USER_ID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPTING_NAME=$(echo $0 | cut -d "." -f2)
-LOG_FILE=/tmp/$SCRIPTING_NAME-$TIMESTAMP.log
+# USER_ID=$(id -u)
+# TIMESTAMP=$(date +%F-%H-%M-%S)
+# SCRIPTING_NAME=$(echo $0 | cut -d "." -f2)
+# LOG_FILE=/tmp/$SCRIPTING_NAME-$TIMESTAMP.log
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-echo "Please enter DB secure password"
-read -s DB_PASSWORD
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+# echo "Please enter DB secure password"
+# read -s DB_PASSWORD
 
-VALIDATE_FUN(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 is $R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2 is $G SUCCESS $N"
-    fi
-}
+# VALIDATE_FUN(){
+#     if [ $1 -ne 0 ]
+#     then
+#         echo -e "$2 is $R FAILURE $N"
+#         exit 1
+#     else
+#         echo -e "$2 is $G SUCCESS $N"
+#     fi
+# }
 
-if [ $USER_ID -eq 0 ]
-then
-    echo -e "$Y INSTALLING PACKAGE $N"
-else
-    echo "NEED TO SUDO USER FOR THIS PACKAGE INSTALLATION"
-    exit 1
-fi
+# if [ $USER_ID -eq 0 ]
+# then
+#     echo -e "$Y INSTALLING PACKAGE $N"
+# else
+#     echo "NEED TO SUDO USER FOR THIS PACKAGE INSTALLATION"
+#     exit 1
+# fi
 
 
-dnf install mysql-server -y >> $LOG_FILE
+# dnf install mysql-server -y >> $LOG_FILE
 
-VALIDATE_FUN $? "INSTALLING MYSQL"
+# VALIDATE_FUN $? "INSTALLING MYSQL"
 
-systemctl enable mysqld >> $LOG_FILE
+# systemctl enable mysqld >> $LOG_FILE
 
-VALIDATE_FUN $? "ENABLED MYSQL"
+# VALIDATE_FUN $? "ENABLED MYSQL"
 
-systemctl start mysqld >> $LOG_FILE
+# systemctl start mysqld >> $LOG_FILE
 
-VALIDATE_FUN $? "START MYSQL"
+# VALIDATE_FUN $? "START MYSQL"
 
-# mysql_secure_installation --set-root-pass ExpenseApp@1 >> $LOG_FILE
+# # mysql_secure_installation --set-root-pass ExpenseApp@1 >> $LOG_FILE
 
-mysql -h 172.31.45.100 -uroot -p${DB_PASSWORD} -e 'SHOW DATABASES;' &>>LOG_FILE
+# mysql -h 172.31.45.100 -uroot -p${DB_PASSWORD} -e 'SHOW DATABASES;' &>>LOG_FILE
 
-if [ $? -eq 0 ]
-then
-    echo "already setup"
-else
-    mysql_secure_installation --set-root-pass ${DB_PASSWORD} &>> $LOG_FILE
-    VALIDATE_FUN $? "SETUP ROOT PASSWORD"
-fi
+# if [ $? -eq 0 ]
+# then
+#     echo "already setup"
+# else
+#     mysql_secure_installation --set-root-pass ${DB_PASSWORD} &>> $LOG_FILE
+#     VALIDATE_FUN $? "SETUP ROOT PASSWORD"
+# fi
 
-mkdir -p rabbani
+# mkdir -p rabbani
 # !/bin/bash
 
 # USERID=$(id -u)
@@ -109,3 +109,64 @@ mkdir -p rabbani
 # else
 #     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 # fi
+
+
+
+
+#!/bin/bash
+
+USER_ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f2)
+LOGFILES=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+echo "setup root password"
+read -s dbpassword
+
+#colors
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE_FUN(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 is $G SUCCESS $N"
+    fi
+}
+
+
+if [ $USER_ID -eq 0 ]
+then 
+    echo "This is SUPER USER GO TO INSTALL PACKAGES"
+else
+    echo "THIS IS NOT SUPER USER PLEASE STOP OF THIS TO INSTALL PACKAGES"
+    exit 1
+fi
+
+dnf install mysql-server -y &>>$LOGFILES
+VALIDATE_FUN $? "INSTALLING MYSQL PACKAGE"
+
+systemctl enable mysqld &>>$LOGFILES
+VALIDATE_FUN $? "Mysql system enabaled"
+
+systemctl start mysqld &>>$LOGFILES
+VALIDATE_FUN $? "Mysql is start"
+
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILES
+# VALIDATE_FUN $? "Password setup to enter the db"
+
+mysql -h 54.237.224.140 -u root -p -p${dbpassword} -e 'SHOW DATABASES;' &>>$LOGFILES
+if [ $? -ne 0 ]
+then 
+    mysql_secure_installation --set-root-pass ${dbpassword} &>>$LOGFILES
+    VALIDATE_FUN $? "setup root password"
+else
+    echo "Already Setup"
+    exit 1
+fi
+    
